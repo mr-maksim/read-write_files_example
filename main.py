6cce5a6fd7dc937c53b0d13314473d8bf47d266a
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-03-27 13:08:56 +0300

    Added function "read_recipt()"

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import __hello__
+def read_recipt():
+
+    cook_book = {}
+    with open('files/recipt.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            dishes = line.strip()
+            print(dishes)
+            cook_book[dishes] = []
+            quantity = int(file.readline())
+            for ingredients in range(quantity):
+                line_split = file.readline().strip().split('|')
+                cook_book[dishes].append(dict([
+                    ('ingridient_name', line_split[0]),
+                    ('quantity', line_split[1]),
+                    ('measure', line_split[2]),
+                ]))
+            file.readline()
+
+
+read_recipt()
