9479f6e8c056ed751f896097f6c17da7934fcbb4
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-03-27 16:31:40 +0300

    Added functions: * menu() * print_main_menu() * dishes_list(cook_book) * clear()

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_PATH = os.getcwd()
+FILE_DIR = 'files'
+FILE_NAME = 'recipt.txt'
+
+RECIPT_PATH = os.path.join(BASE_PATH, FILE_DIR, FILE_NAME)
--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-def read_recipt():
+from os import system, name
+import config
 
+
+def menu():
+    # print(f'{"_"*15}Добро пожаловать{"_"*15}')
+    print_main_menu()
+    while True:
+        try:
+            choose = int(input('Введите номер нужного пункта: '))
+            break
+        except:
+            print("Вы ввели некорректные данные")
+    if choose == 1:
+        clear()
+        dishes_list(open_recipt(config.RECIPT_PATH))
+    elif choose == 0:
+        exit()
+
+
+def print_main_menu():
+    menu_list = ["1 - Просмотр списка блюд", "0 - Exit"]
+    for item in menu_list:
+        print(item)
+
+
+def dishes_list(cook_book):
+    for num, item in enumerate(cook_book.keys()):
+        print(f'{num + 1}) {item}')
+    print('\n')
+    menu()
+
+
+def open_recipt(recipt_path):
     cook_book = {}
-    with open('files/recipt.txt', 'r', encoding='utf-8') as file:
+    with open(recipt_path, 'r', encoding='utf-8') as file:
         for line in file:
             dishes = line.strip()
-            print(dishes)
             cook_book[dishes] = []
             quantity = int(file.readline())
             for ingredients in range(quantity):
@@ -15,6 +46,15 @@ def read_recipt():
                     ('measure', line_split[2]),
                 ]))
             file.readline()
+    return cook_book
 
 
-read_recipt()
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
+if __name__ == "__main__":
+    menu()
